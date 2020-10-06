62a15595f50d9de0f7d7a9db06791371eefbcf68
Giuseppe Campanelli <suprememilanfan@gmail.com>
2020-10-05 22:00:22 -0400

    Added support for more parameters

--- api.py
+++ api.py
@@ -12,5 +12,23 @@ def find_flights(from_, to_, start_, end_):
     
     return ff.find_flights()
 
+@app.route('/flights/<from_>/<to_>/<start_>/<end_>/<passengers_>')
+def find_flights_passengers(from_, to_, start_, end_, passengers_):
+    ff = FlightFinder(from_, to_, start_, end_, passengers_)
+    
+    return ff.find_flights()
+
+@app.route('/flights/<from_>/<to_>/<start_>/<end_>/<stops_>')
+def find_flights_stops(from_, to_, start_, end_, stops_):
+    ff = FlightFinder(from_, to_, start_, end_, None, stops_)
+    
+    return ff.find_flights()
+
+@app.route('/flights/<from_>/<to_>/<start_>/<end_>/<passengers_>/<stops_>')
+def find_flights_passengers_stops(from_, to_, start_, end_, stops_):
+    ff = FlightFinder(from_, to_, start_, end_, passengers_, stops_)
+    
+    return ff.find_flights()
+
 if __name__ == '__main__':
     app.run(debug=True)
